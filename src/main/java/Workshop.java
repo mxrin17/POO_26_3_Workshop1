3a33fa53fdb0af23325dd749baf14cacb62f313f
Alejandro Marín <lamarinv@udistrital.edu.co>
2026-09-20 22:55:50 -0500

    promedioElementos

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -89,10 +89,11 @@ public class Workshop {
 
     // Método que calcula el promedio de los elementos de un arreglo
     public double promedioElementos(int[] arreglo) {
-        // TODO: Implementar el método para calcular el promedio de los elementos de un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
+    if (arreglo == null || arreglo.length == 0) {
         return 0.0;
     }
+    return (double) sumaElementos(arreglo) / arreglo.length;
+}
 
     // Método que encuentra el elemento mayor en un arreglo
     public int encontrarElementoMayor(int[] arreglo) {
