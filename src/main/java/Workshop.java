a4bea7df0ab8c6dc29d7bfa71914f3e8ab7da7e3
Alejandro Marín <lamarinv@udistrital.edu.co>
2026-09-20 22:57:12 -0500

    encontrarElementoMayor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -97,10 +97,17 @@ public class Workshop {
 
     // Método que encuentra el elemento mayor en un arreglo
     public int encontrarElementoMayor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
+    if (arreglo == null || arreglo.length == 0) {
+        throw new IllegalArgumentException("El arreglo no puede ser nulo ni estar vacío.");
     }
+    int mayor = arreglo[0];
+    for (int i = 1; i < arreglo.length; i++) {
+        if (arreglo[i] > mayor) {
+            mayor = arreglo[i];
+        }
+    }
+    return mayor;
+}
 
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
