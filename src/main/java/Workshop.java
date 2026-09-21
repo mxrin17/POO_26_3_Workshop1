4935cd25800a759bfd9b5a31dc7bd8bccc5c513e
Alejandro Marín <lamarinv@udistrital.edu.co>
2026-09-20 23:13:51 -0500

    contarPalabras

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -283,10 +283,12 @@ public class Workshop {
 
     // Método que cuenta el número de palabras en una cadena
     public int contarPalabras(String cadena) {
-        // TODO: Implementar el método para contar el número de palabras en una cadena.
-        // Ejemplo: Si cadena = "Este es un test", el resultado debería ser 4.
+    if (cadena == null || cadena.trim().isEmpty()) {
         return 0;
     }
+    String[] palabras = cadena.trim().split("\\s+");
+    return palabras.length;
+}
 
     // Método que convierte una cadena a mayúsculas
     public String convertirAMayusculas(String cadena) {
