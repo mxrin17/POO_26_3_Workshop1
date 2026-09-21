9872f88c7bf860394f2ddaac0fcfc993e70a015d
Alejandro Marín <lamarinv@udistrital.edu.co>
2026-09-21 01:04:41 -0500

    pruebaAreaCirculo3

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -467,7 +467,7 @@ public String jugarPiedraPapelTijeraLagartoSpock(String eleccionUsuario) {
 
    public double areaCirculo(double radio) {
     if (radio < 0) {
-        return 0.0;
+        throw new IllegalArgumentException("El radio no puede ser negativo.");
     }
     return Math.PI * Math.pow(radio, 2);
 }
